ec1bb781d546d40e3afb0132711f1614208f8d2c
Mike <officiallooking@gmail.com>
2010-07-08 12:54:32 -0500

    minor: whitespace

--- lib/organization_puller.rb
+++ lib/organization_puller.rb
@@ -13,24 +13,23 @@ class OrganizationPuller < Puller
     @index_data     = Output.file '/../cache/raw/organization/index.yml'
     @index_html     = Output.file '/../cache/raw/organization/index.html'
     @bonus_html     = Output.file '/../cache/raw/organization/bonus.html'
-   # @pull_log       = Output.file '/../cache/raw/source/pull_log.yml'
     super
   end
 
   protected
 
   def get_metadata(doc)
-	metadata=[]
-	links_blocks=doc.xpath('//a[@class="bodylinks"]')
+	metadata = []
+	links_blocks = doc.xpath('//a[@class="bodylinks"]')
 
 	links_blocks.each do |link_block|
-			name=U.single_line_clean(link_block.inner_text)
-      link=URI.unescape(link_block["href"])
+			name = U.single_line_clean(link_block.inner_text)
+      link = URI.unescape(link_block["href"])
 
-      link="http://michigan.gov"+link
-			metadata<<{
-				:name=>name,
-				:href=>link,
+      link = "http://michigan.gov" + link
+			metadata << {
+				:name => name,
+				:href => link,
 			}
 	end
   #Adds organizations that are not included in the organization page but are used as sources
@@ -69,21 +68,21 @@ class OrganizationPuller < Puller
 
   def append_bonus_orgs(metadata)
       doc = U.parse_html_from_file_or_uri(@bonus_uri, @bonus_html, :force_fetch => true)
-      paragraphs=doc.xpath("//p[@style='padding-left: 7pt; text-indent: -8pt; margin-bottom: 0; margin-top: 0;']")
+      paragraphs = doc.xpath("//p[@style='padding-left: 7pt; text-indent: -8pt; margin-bottom: 0; margin-top: 0;']")
 
-      paragraphs.each do |p|
-        a_tag=p.css("a").first
-        link=a_tag["href"]
+      paragraphs.each do | p |
+        a_tag = p.css("a").first
+        link = a_tag["href"]
         #If local link make appropraite changes
-        link="http://michigan.gov"+link if link.match(/^\//)
+        link = "http://michigan.gov" + link if link.match(/^\//)
         #Add http:// if it isn't there
-        link="http://"+link if link.match(/^http:\/\//).nil?
+        link = "http://"+link if link.match(/^http:\/\//).nil?
      
-        url=link.scan(/^http:\/\/.*?\//).first
-        org_name=url.gsub("http://","").chop
+        url = link.scan(/^http:\/\/.*?\//).first
+        org_name = url.gsub("http://","").chop
         
-        match=metadata.find {|m| url==m[:href]}
-        metadata<<{:name=>org_name,:href=>url} if match.nil?
+        match = metadata.find { | m | url == m[:href] }
+        metadata << { :name => org_name, :href => url } if match.nil?
       end
   end
 end
